31560362db60f6050de0e8a3a6799d7b16211583
sametcelikbicak <sametcelikbicak@outlook.com>
2022-04-07 09:51:40 +0300

    Rename parameter

--- src/app/decorators/log.decorator.ts
+++ src/app/decorators/log.decorator.ts
@@ -1,15 +1,15 @@
 export function Log(): MethodDecorator {
-  return function (target: Function, key: string, descriptor: any) {
+  return function (target: Function, propertyKey: string, descriptor: any) {
     const originalMethod = descriptor.value;
 
     descriptor.value = function (...args: any[]) {
-      console.log(` -> Entering ${key} method`);
+      console.log(` |-> Entering '${propertyKey}' method`);
       console.log(
-        `%c ${key} method args:${args}`,
+        `%c "${propertyKey}" method args:${args}`,
         'background:green;color:white'
       );
       const result = originalMethod.apply(this, args);
-      console.log(` <- Leaving ${key} method`);
+      console.log(` <-| Leaving '${propertyKey}' method`);
 
       return result;
     };
